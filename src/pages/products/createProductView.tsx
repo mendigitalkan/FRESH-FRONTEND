5c445d1afe6b21100285574dc3c0870285f05938
misdarmanto <misdarmanto1922@gmail.com>
2024-01-22 16:21:19 +0700

    _

--- src/pages/products/createProductView.tsx
+++ src/pages/products/createProductView.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 import {
   Button,
   Card,
@@ -11,45 +11,45 @@ import {
   MenuItem,
   Grid,
   InputLabel,
-  FormControl
-} from '@mui/material'
-import { useNavigate } from 'react-router-dom'
-import { useHttp } from '../../hooks/http'
-import { IProductCreateRequestModel } from '../../models/productsModel'
-import { ICategoryModel } from '../../models/categoryModel'
-import { handleUploadImageToFirebase } from '../../utilities/uploadImageToFirebase'
-import ModalVariantProductView from './modalVariantProduct'
+  FormControl,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useHttp } from "../../hooks/http";
+import { IProductCreateRequestModel } from "../../models/productsModel";
+import { ICategoryModel } from "../../models/categoryModel";
+import { handleUploadImageToFirebase } from "../../utilities/uploadImageToFirebase";
 
 export default function CreateProductView() {
-  const { handlePostRequest, handleGetRequest } = useHttp()
-  const navigate = useNavigate()
+  const { handlePostRequest, handleGetRequest } = useHttp();
+  const navigate = useNavigate();
 
-  const [productName, setProductName] = useState('')
-  const [productDescription, setProductDescription] = useState('')
-  const [productImages, setProductImages] = useState<string[]>([])
-  const [productPrice, setProductPrice] = useState(0)
-  const [productCategoryId, setProductCategoryId] = useState('')
-  const [productStock, setProductStock] = useState(0)
-  const [productVariant, setProductVariant] = useState('')
+  const [productName, setProductName] = useState("");
+  const [productDescription, setProductDescription] = useState("");
+  const [productImages, setProductImages] = useState<string[]>([]);
+  const [productPrice, setProductPrice] = useState(0);
+  const [productCategoryId, setProductCategoryId] = useState("");
+  const [productStock, setProductStock] = useState(0);
+  const [productVariant, setProductVariant] = useState("");
 
-  const [openModalVariantProduct, setOpenModalVariantProduct] = useState(false)
-
-  const [categories, setCategories] = useState<ICategoryModel[]>([])
+  const [categories, setCategories] = useState<ICategoryModel[]>([]);
 
   const getCategories = async () => {
     const result = await handleGetRequest({
-      path: '/categories'
-    })
-    setCategories(result.items)
-  }
+      path: "/categories",
+    });
+    setCategories(result.items);
+  };
 
   const handleUploadImage = (event: any) => {
-    const image = event.target.files[0]
+    const image = event.target.files[0];
     handleUploadImageToFirebase({
       selectedFile: image,
-      getImageUrl: (image) => setProductImages([...productImages, image])
-    })
-  }
+      getImageUrl: (image) => setProductImages([...productImages, image]),
+    });
+  };
 
   const handleSubmit = async () => {
     try {
@@ -60,101 +60,110 @@ export default function CreateProductView() {
         productPrice,
         productCategoryId,
         productStock,
-        productVariant
-      }
+        productVariant,
+      };
 
       await handlePostRequest({
-        path: '/products',
-        body: payload
-      })
+        path: "/products",
+        body: payload,
+      });
 
-      navigate('/products')
+      navigate("/products");
     } catch (error: unknown) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   useEffect(() => {
-    getCategories()
-  }, [])
+    getCategories();
+  }, []);
 
   return (
     <Card
       sx={{
         mt: 5,
-        p: { xs: 3, md: 5 }
+        p: { xs: 3, md: 5 },
       }}
     >
-      <Typography variant='h4' marginBottom={5} color='primary' fontWeight={'bold'}>
+      <Typography
+        variant="h4"
+        marginBottom={5}
+        color="primary"
+        fontWeight={"bold"}
+      >
         Tambah Product
       </Typography>
 
-      <Typography color={'gray'}>Info Product</Typography>
       <Box
-        component='form'
+        component="form"
         style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center'
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
         }}
       >
+        <Typography fontWeight={"bold"} my={2}>
+          Info Product
+        </Typography>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
             <TextField
-              label='Nama'
-              id='outlined-start-adornment'
+              label="Nama"
+              id="outlined-start-adornment"
               fullWidth
               value={productName}
-              type='text'
+              type="text"
               onChange={(e) => {
-                setProductName(e.target.value)
-              }}
-            />
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              label='Deskripsi'
-              id='outlined-start-adornment'
-              fullWidth
-              value={productDescription}
-              type='text'
-              onChange={(e) => {
-                setProductDescription(e.target.value)
+                setProductName(e.target.value);
               }}
             />
           </Grid>
 
           <Grid item xs={12} sm={6}>
             <TextField
-              label='Harga'
+              label="Harga: masukan dalam angka tanpa persen (Rp)"
               fullWidth
-              id='outlined-start-adornment'
+              id="outlined-start-adornment"
               value={productPrice}
-              type='number'
+              type="number"
               onChange={(e) => {
-                setProductPrice(+e.target.value)
+                setProductPrice(+e.target.value);
               }}
             />
           </Grid>
           <Grid item xs={12} sm={6}>
             <TextField
-              label='Stok'
+              label="Diskon: masukan dalam angka tanpa persen (%)"
               fullWidth
-              id='outlined-start-adornment'
-              value={productStock}
-              type='number'
+              id="outlined-start-adornment"
+              value={productPrice}
+              type="number"
               onChange={(e) => {
-                setProductStock(+e.target.value)
+                setProductPrice(+e.target.value);
+              }}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Stok"
+              fullWidth
+              id="outlined-start-adornment"
+              value={productStock}
+              type="number"
+              onChange={(e) => {
+                setProductStock(+e.target.value);
               }}
             />
           </Grid>
 
           <Grid item xs={12} sm={6}>
             <FormControl fullWidth>
-              <InputLabel id='demo-controlled-open-select-label'>Kategori</InputLabel>
+              <InputLabel id="demo-controlled-open-select-label">
+                Kategori
+              </InputLabel>
               <Select
-                labelId='demo-select-small-label'
-                id='demo-select-small'
+                labelId="demo-select-small-label"
+                id="demo-select-small"
                 fullWidth
                 value={productCategoryId}
                 onChange={(e) => setProductCategoryId(e.target.value)}
@@ -170,9 +179,9 @@ export default function CreateProductView() {
         </Grid>
 
         <Box sx={{ my: 3 }}>
-          <Typography color={'gray'}>Foto Product</Typography>
-          <Stack direction={'row'} flexWrap='wrap' spacing={2}>
-            <TextField fullWidth type='file' onChange={handleUploadImage} />
+          <Typography color={"gray"}>Foto Product</Typography>
+          <Stack direction={"row"} flexWrap="wrap" spacing={2}>
+            <TextField fullWidth type="file" onChange={handleUploadImage} />
             {productImages.map((image, index) => (
               <img
                 key={index}
@@ -180,35 +189,48 @@ export default function CreateProductView() {
                 style={{
                   marginTop: 10,
                   width: 200,
-                  height: 200
+                  height: 200,
                 }}
               />
             ))}
           </Stack>
         </Box>
 
-        <Box sx={{ mt: 3 }}>
-          <Typography color={'gray'}>Varian Product</Typography>
-          <Button onClick={() => setOpenModalVariantProduct(true)} variant='outlined'>
-            Tambah Varian Produk
-          </Button>
-          <ModalVariantProductView
-            open={openModalVariantProduct}
-            setOpen={setOpenModalVariantProduct}
-            onSelect={setProductVariant}
-          />
+        <Box sx={{ my: 3 }}>
+          <Typography fontWeight={"bold"} mb={2}>
+            Deskripsi
+          </Typography>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Deskripsi"
+              id="outlined-start-adornment"
+              multiline
+              fullWidth
+              rows={4}
+              value={productDescription}
+              type="text"
+              onChange={(e) => {
+                setProductDescription(e.target.value);
+              }}
+            />
+          </Grid>
         </Box>
 
-        <Stack direction={'row'} justifyContent='flex-end'>
+        <Box sx={{ mt: 3 }}>
+          <Typography fontWeight={"bold"}>Varian Product</Typography>
+          <VariantProductSection />
+        </Box>
+
+        <Stack direction={"row"} justifyContent="flex-end">
           <Button
             sx={{
               my: 1,
-              width: '25ch',
-              backgroundColor: 'dodgerblue',
-              color: '#FFF',
-              fontWeight: 'bold'
+              width: "25ch",
+              backgroundColor: "dodgerblue",
+              color: "#FFF",
+              fontWeight: "bold",
             }}
-            variant={'contained'}
+            variant={"contained"}
             onClick={handleSubmit}
           >
             Submit
@@ -216,5 +238,60 @@ export default function CreateProductView() {
         </Stack>
       </Box>
     </Card>
-  )
+  );
 }
+
+interface IVariantTypes {
+  type: string;
+  values: string[];
+}
+
+const VariantProductSection = () => {
+  const [variantList, setVariantList] = useState<IVariantTypes[]>([]);
+
+  return (
+    <>
+      <Typography color={"gray"}>Ukuran</Typography>
+      <Stack spacing={2}>
+        <FormGroup>
+          <Stack direction={"row"} flexWrap={"wrap"} spacing={2}>
+            <FormControlLabel
+              onChange={(e) => setVariantList([...{ vari }])}
+              control={<Checkbox />}
+              label="S"
+            />
+            <FormControlLabel control={<Checkbox />} label="M" />
+            <FormControlLabel control={<Checkbox />} label="L" />
+            <FormControlLabel control={<Checkbox />} label="XL" />
+            <FormControlLabel control={<Checkbox />} label="XXL" />
+          </Stack>
+        </FormGroup>
+        <Typography color={"gray"}>Warna</Typography>
+        <FormGroup>
+          <Stack direction={"row"} flexWrap={"wrap"} spacing={2}>
+            <FormControlLabel control={<Checkbox />} label="Hitam" />
+            <FormControlLabel control={<Checkbox />} label="Merah" />
+            <FormControlLabel control={<Checkbox />} label="Putih" />
+            <FormControlLabel control={<Checkbox />} label="Kuning" />
+            <FormControlLabel control={<Checkbox />} label="Hijau" />
+            <FormControlLabel control={<Checkbox />} label="Biru" />
+            <FormControlLabel control={<Checkbox />} label="Abu-Abu" />
+          </Stack>
+        </FormGroup>
+        <Typography color={"gray"}>Kondisi</Typography>
+        <FormGroup>
+          <Stack direction={"row"} flexWrap={"wrap"} spacing={2}>
+            <FormControlLabel control={<Checkbox />} label="Baru" />
+            <FormControlLabel control={<Checkbox />} label="Bekas" />
+          </Stack>
+        </FormGroup>
+        <Typography color={"gray"}>Berat</Typography>
+        <TextField
+          label="Berat: masukan angkat dalam gram tanpa (g)"
+          id="outlined-start-adornment"
+          type="number"
+        />
+      </Stack>
+    </>
+  );
+};
